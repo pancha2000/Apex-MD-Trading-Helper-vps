c55fe151d02687d02b789ad263827d36d313d8fc
pancha2000 <cdilrukshi52@gmail.com>
2026-01-29 16:34:34 +0530

    Update ping.js

--- plugin/ping.js
+++ plugin/ping.js
@@ -1,27 +1,20 @@
-// plugins/ping.js
+const { cmd } = require('../command');
 
-const config = require('../config')
-let fs = require('fs')
-const { cmd, commands } = require('../command')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 cmd({
     pattern: "ping",
-    react: "🤖",
-    alias: ["speed"],
-    desc: "Check bot\'s ping",
+    desc: "Check bot's response time.",
     category: "main",
-    // use: '.ping', // මේ පේලිය අයින් කරන්න, නැත්නම් මේ විදියට comment කරන්න
+    react: "⚡",
     filename: __filename
 },
-async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-var inital = new Date().getTime();
-let ping = await conn.sendMessage(from , { text: '*_ping is very bad sudda🥺_*'  }, { quoted: mek } )
-var final = new Date().getTime();
-await conn.sendMessage(from, { delete: ping.key })
-return await conn.sendMessage(from , { text: '*🔥ping*\n *' + (final - inital) + ' ms* '  }, { quoted: mek } )
-} catch (e) {
-reply('*Error !!*')
-l(e)
-}
-})
+async (conn, mek, m, { from, reply }) => {
+    try {
+        const start = new Date().getTime();
+        const end = new Date().getTime();
+        const ping = end - start;
+        await reply(`*Pong!* ⚡\nSpeed: ${ping}ms`);
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e}`);
+    }
+});
